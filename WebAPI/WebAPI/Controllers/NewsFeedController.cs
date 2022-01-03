2bfa9fa0f7c924fee1ef134b38189b1a8b4727f9
YasiruUOK <yasirukavinda89@gmail.com>
2022-01-03 23:26:30 +0530

    first commit

---
+++ WebAPI/WebAPI/Controllers/NewsFeedController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class NewsFeedController : ApiController
+    {
+        DataTable dt = new DataTable();
+        static string ConnectionString = ConfigurationManager.ConnectionStrings["localNetwork"].ConnectionString;
+
+        [Route("api/getAllNews")]
+        [HttpGet]
+        public List<NewsFeed> getAllNews()
+        {
+            List<NewsFeed> newsFeedList = new List<NewsFeed>();
+
+            DataSet ds = SqlHelper.ExecuteDataset(ConnectionString
+                     , "GetAllNews"
+                     );
+
+            int count = ds.Tables[0].Rows.Count;
+            // string result = JsonConvert.SerializeObject(ds);
+            //return ds;
+            if (count >= 0)
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    NewsFeed newsFeed = new NewsFeed();
+                    newsFeed.ID = Int32.Parse(ds.Tables[0].Rows[i][0].ToString());
+                    newsFeed.Topic = ds.Tables[0].Rows[i][1].ToString();
+                    newsFeed.News = ds.Tables[0].Rows[i][2].ToString();
+                    newsFeed.Added_Date = DateTime.Parse(ds.Tables[0].Rows[i][3].ToString());
+                    newsFeed.Image_URL = ds.Tables[0].Rows[i][4].ToString();
+                    newsFeedList.Add(newsFeed);
+                }
+            }
+            return newsFeedList;
+        }
+    }
+}
---
+++ WebAPI/WebAPI/Models/NewsFeed.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class NewsFeed
+    {
+        public int ID { get; set; }
+        public string Topic { get; set; }
+        public string News { get; set; }
+        public DateTime Added_Date { get; set; }
+        public string Image_URL { get; set; }
+    }
+}
