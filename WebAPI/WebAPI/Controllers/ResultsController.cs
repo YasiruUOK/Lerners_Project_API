d1b4e415e6919fa599adca9ae3e4e68772148331
YasiruUOK <yasirukavinda89@gmail.com>
2022-01-12 12:57:23 +0530

    Add more items to search results

--- WebAPI/WebAPI/Controllers/ResultsController.cs
+++ WebAPI/WebAPI/Controllers/ResultsController.cs
@@ -36,6 +36,27 @@ namespace WebAPI.Controllers
                 {
                     results.IndexNumber = Int32.Parse(ds.Tables[0].Rows[i][0].ToString());
                     results.Result = ds.Tables[0].Rows[i][1].ToString();
+                    results.FullName = ds.Tables[0].Rows[i][2].ToString();
+                    results.NameWithInitials = ds.Tables[0].Rows[i][3].ToString();
+                    results.Address = ds.Tables[0].Rows[i][4].ToString();
+                    results.ValidTill = DateTime.Parse(ds.Tables[0].Rows[i][5].ToString());
+                    results.ValidTillDateFormat = results.ValidTill.ToString("dd/MM/yyyy");
+                    results.CategoryA = bool.Parse(ds.Tables[0].Rows[i][6].ToString());
+                    results.CategoryB = bool.Parse(ds.Tables[0].Rows[i][7].ToString());
+                    results.CategoryC = bool.Parse(ds.Tables[0].Rows[i][8].ToString());
+                    results.CategoryList = "";
+                    if (results.CategoryA)
+                    {
+                        results.CategoryList = results.CategoryList + "A ";
+                    }
+                    if (results.CategoryB)
+                    {
+                        results.CategoryList = results.CategoryList + "B ";
+                    }
+                    if (results.CategoryC)
+                    {
+                        results.CategoryList = results.CategoryList + "C";
+                    }
                 }
             }
             if (results.IndexNumber == 0)
--- WebAPI/WebAPI/Models/Results.cs
+++ WebAPI/WebAPI/Models/Results.cs
@@ -7,7 +7,18 @@ namespace WebAPI.Models
 {
     public class Results
     {
+        
+
         public int IndexNumber { get; set; }
         public string Result { get; set; }
+        public string FullName { get; set; }
+        public string NameWithInitials { get; set; }
+        public string Address { get; set; }
+        public DateTime ValidTill { get; set; }
+        public bool CategoryA { get; set; }
+        public bool CategoryB { get; set; }
+        public bool CategoryC { get; set; }
+        public string ValidTillDateFormat { get; set; }
+        public string CategoryList { get; set; }
     }
 }
